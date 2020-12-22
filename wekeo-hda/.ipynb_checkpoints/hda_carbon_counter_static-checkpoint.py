411c185b95b1f124494285c088c29e97a23f92e9
Mouad <mouadattaqi9@gmail.com>
2020-12-22 13:14:49 +0000

    update file with advanced features

--- wekeo-hda/.ipynb_checkpoints/hda_carbon_counter_static-checkpoint.py
+++ wekeo-hda/.ipynb_checkpoints/hda_carbon_counter_static-checkpoint.py
@@ -5,25 +5,48 @@ import json
 import urllib.parse
 from hda_api_functions import *
 import netCDF4
-
-
-import requests, warnings
-warnings.filterwarnings('ignore')
-
-dataset_id = "EO:ECMWF:DAT:CAMS_GREENHOUSE_GAS_FLUXES"
-
-username = 'mattaqi'
-password = 'masma3991'
-api_key = generate_api_key(username, password)
-download_dir_path = os.getcwd()
+import numpy as np
 
 # Initialisation
-fn = './ressources/cams73_latest_co2_flux_satellite_mm_201910.nc'
+fn = './ressources/cams73_latest_co2_flux_satellite_mm_201912.nc'
 ds = netCDF4.Dataset(fn)
-long = ds['longitude']
-lat = ds['latitude']
+print(ds)
+print ("------------------------------------------------------------")
+print ("variables are: =>")
+#for i in ds.variables:
+#    print (i, ds.variables[i].units, ds.variables[i].shape)
+print ("------------------------------------------------------------")
+#for i in ds.variables:
+#   print (ds.variables[i])
+#"npor"    
+long = ds['longitude'][:]
+lat = ds['latitude'][:]
 
+long_max = 10
+long_min = 0
+index_long_max = np.where(long_max <= long)[0]
+print("index_long_max")
+print(index_long_max)
+print("stat")
+print(index_long_max[0], long[index_long_max[0]])
+
+
+area = ds['area'][0:10, 0:10]
+lsf = ds['lsf'][3:6,3:4]
+flux_foss = ds['flux_foss'][:]
+print ("------------------------------------------------------------")
+print ("flux_foss is: =>")
+print (flux_foss)
+print ("------------------------------------------------------------")
+print ("lsf is: =>")
+print (lsf)
+print ("------------------------------------------------------------")
+print ("area is: =>")
+print (area)
+print ("------------------------------------------------------------")
+print ("long is: =>")
 print (long)
+print ("------------------------------------------------------------")
+print ("lat is: =>")
 print (lat)
-for i in ds.variables:
-    print (i, ds.variables[i].units, ds.variables[i].shape)
+
